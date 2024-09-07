5ead516f0612103bfa5a04b1b33a4f1c01488a12
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-07 18:26:04 -0300

    - Adicionado os componentes botão, header e input no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { PlusCircle } from '@phosphor-icons/react'
+import {Header} from "./components/Header.tsx";
+import {Button} from "./components/Button.tsx";
+import { Input } from "./components/Input.tsx";
+
 function App() {
 
+    function handleAddTask(){
+        console.log("Task added");
+    }
+
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
+    <main>
+        <Header />
+        <Input />
+        <h1>Hello World</h1>
+        <Button onClick={handleAddTask}>
+            Criar
+            <PlusCircle size={16} color="#f2f2f2" weight="bold" />
+        </Button>
+
+    </main>
   )
 }
 
