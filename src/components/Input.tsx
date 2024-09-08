fe1bbf4ea8ed5930bccd0bd36c98943e9ef8a3cb
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-07 22:07:58 -0300

    - Criado os componentes Input.module.css e Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,26 @@
+import styles from "./Input.module.css";
 
-export function Input() {
-    return(
-        <h1>Adicione uma nova tarefa</h1>
-    )
+export function Input({
+  ...rest
+}: React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>) {
+  return (
+    <input
+      className={styles.container}
+      placeholder="Adicione uma nova tarefa"
+      {...rest}
+    />
+  );
 }
+
+//Input é uma função que recebe um objeto de propriedades (props).
+
+// As propriedades são desestruturadas utilizando o operador spread ...rest,
+// significando que todas as propriedades passadas ao componente Input serão incluídas no objeto rest.
+
+// O tipo das propriedades esperadas é definido usando React.DetailedHTMLProps
+// com React.InputHTMLAttributes<HTMLInputElement> e HTMLInputElement.
+// Isso significa que todas as propriedades padrão de um elemento <input> HTML
+// são aceitas por este componente.
