fa2e75eec73bbb025c311e6c4bfda4a38721abf0
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-07 22:07:14 -0300

    - Adicionado o componente Input.tsx - Removido o Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import { PlusCircle } from '@phosphor-icons/react'
-import {Header} from "./components/Header.tsx";
-import {Button} from "./components/Button.tsx";
+import { PlusCircle } from "@phosphor-icons/react";
+import { Header } from "./components/Header.tsx";
+import { Button } from "./components/Button.tsx";
 import { Input } from "./components/Input.tsx";
+import { useState } from "react";
+
+import styles from './App.module.css'
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
 
-    function handleAddTask(){
-        console.log("Task added");
+  function handleAddTask() {
+    if (!inputValue) {
+      return;
     }
+  }
 
   return (
     <main>
-        <Header />
-        <Input />
-        <h1>Hello World</h1>
-        <Button onClick={handleAddTask}>
+      <Header />
+      <section className={styles.content}>
+        <div className={styles.container}>
+          <Input
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+            value={inputValue}
+          />
+          <Button onClick={handleAddTask}>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
-        </Button>
-
+          </Button>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
